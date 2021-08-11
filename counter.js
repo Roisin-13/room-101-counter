b6016c97c0856233ca60a8a77034e2e1177cc6da
Roisin-13 <86969519+Roisin-13@users.noreply.github.com>
2021-08-11 18:10:02 +0100

    simple ++ function, and variables renamed

--- counter.js
+++ counter.js
@@ -1,9 +1,10 @@
-let number = document.getElementById("numChange");
+let numChange = document.getElementById("numChange");
 number = 0;
 
 let up = document.querySelector(".btnUp");
 let down = document.querySelector(".btnDown");
 
+/*
 numUp = () => {
     for (let i = 0; i < 100; i++);
     return i;
@@ -13,7 +14,13 @@ numDown = () => {
     for (let i = 0; i > -100; i--);
     return i;
 }
+*/
+up.addEventListener ("click", numUp = () => {
+    number++;
+    numChange.innerHTML = number;
+});
 
-up.addEventListener ("click", numUp());
-
-down.addEventListener ("click", numDown());
+down.addEventListener ("click", numDown = () => {
+    number--;
+    numChange.innerHTML = number;
+});
