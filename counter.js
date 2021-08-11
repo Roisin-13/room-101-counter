17ae9e0ed43b412777e31208e068f97f44c3c313
Roisin-13 <86969519+Roisin-13@users.noreply.github.com>
2021-08-11 17:07:27 +0100

    still working on js - maybe change for loop for something simple

--- counter.js
+++ counter.js
@@ -1,1 +1,19 @@
 let number = document.getElementById("numChange");
+number = 0;
+
+let up = document.querySelector(".btnUp");
+let down = document.querySelector(".btnDown");
+
+numUp = () => {
+    for (let i = 0; i < 100; i++);
+    return i;
+}
+
+numDown = () => {
+    for (let i = 0; i > -100; i--);
+    return i;
+}
+
+up.addEventListener ("click", numUp());
+
+down.addEventListener ("click", numDown());
